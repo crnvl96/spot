f02cd959c6704d4087af55ec58ff5b8f92c6fc0d
Adran Carnavale <adran@hey.com>
2025-10-18 12:35:20 -0300

    add some minimal documentation at the main package

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
+// Package main is the entry point for the spot CLI tool.
 package main
 
 import "github.com/crnvl96/spot/internal"
+
+// main runs the spot CLI application.
+//
+// spot is a cli tool to check if you have uncommited changes in any of your repos
 
 func main() {
 	internal.Execute()
